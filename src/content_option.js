6eaf4808a6b6e2e1896182a3b7a82240d02a6165
vinita poonia <vinitapoonia333@gmail.com>
2024-01-08 22:15:10 +0530

    initial commit

---
+++ src/content_option.js
@@ -0,0 +1,153 @@
+
+
+const logotext = "Vinita";
+const meta = {
+    title: "Vinita",
+    description: "I am Vinita software Developer _ Full stack devloper,currently a finaly year student",
+};
+
+const introdata = {
+    title: "I’m Vinita",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop logics",
+    },
+    description: "Looking for 6 month Internship and full time SDE role, graduating in 2024. Final year Student at Army Institute of Technology, Pune. Skills:  C++ ,ReactJS, JavaScript, Python, TypeScript, Django, SQL , MongoDB , HTML5, CSS3, Tailwind CSS, and NodeJS",
+    your_img_url: "download.png",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I have experience in   C++ ,ReactJS, JavaScript, Python, TypeScript, Django, SQL , MongoDB , HTML5, CSS3, Tailwind CSS, and NodeJS.Committed to continuous learning and do seek new opportunities to contribute to the organization. Let's explore how we can work together for unity and progress.",
+};
+const worktimeline = [{
+    jobtitle: "Software Engineer Intern",
+    where: "Microsoft",
+    date: "10 April 2023 - 2 June 2023",
+},
+{
+    jobtitle: "Technology Intern",
+    where: "Teach For India",
+    date: "20 February 2023 - 28 March 2023",
+},
+
+];
+
+const skills = [{
+    name: "Python",
+    value: 90,
+},
+{
+    name: "Djano",
+    value: 85,
+},
+{
+    name: "Javascript",
+    value: 80,
+},
+{
+    name: "React",
+    value: 60,
+},
+{
+    name: "Jquery",
+    value: 85,
+},
+];
+
+const services = [{
+    title: "UI & UX Design",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
+{
+    title: "Wordpress Design",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
+];
+
+const dataportfolio = [{
+    img: "https://private-user-images.githubusercontent.com/77224604/294952017-7cbdf028-9985-4870-80de-eabea2642baa.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDQ3MjgzNTIsIm5iZiI6MTcwNDcyODA1MiwicGF0aCI6Ii83NzIyNDYwNC8yOTQ5NTIwMTctN2NiZGYwMjgtOTk4NS00ODcwLTgwZGUtZWFiZWEyNjQyYmFhLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAxMDglMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMTA4VDE1MzQxMlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWNmNzIzZDk4YTI0NjYyMmY5OGQzYmQ1Njc3NGIyMWYxZTk0Y2Q4ZTIwZjQwNmViMDlhNmFlYTAxZjUyOGY3ZTEmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.s7sdnbrHttriu45yo7wQOtspTDJ_oiXyAg6bprsxiFg",
+    description: "CodeBoard: A Code editor and white board for programming and head scracthing",
+    link: "https://code-board-adg1.vercel.app/",
+},
+
+ 
+{
+    img: "https://private-user-images.githubusercontent.com/77224604/294950494-e84c18ee-fb85-48c0-a4ae-cddb02a5f590.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDQ3MjgwNDksIm5iZiI6MTcwNDcyNzc0OSwicGF0aCI6Ii83NzIyNDYwNC8yOTQ5NTA0OTQtZTg0YzE4ZWUtZmI4NS00OGMwLWE0YWUtY2RkYjAyYTVmNTkwLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAxMDglMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMTA4VDE1MjkwOVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTM4Y2FhNTBlNjM0N2M4MWY0N2FmOWI4M2FkYTIzNWVjYjI5MTY4MDQ1N2RiOGIwMTIxYTA0MWVkZWUzYzcyZmYmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.HlnJ4CTjbYnL6iRWXdJmpi0Elq1H46cS2ozievMMLt0",
+    description: "Engage: A platform that provides interactive UI representation of a Car dataset insights",
+    link: "https://github.com/Deaddemon/engage_mentee/tree/master",
+},
+{
+    img: "https://private-user-images.githubusercontent.com/77224604/294949485-cc8f83b3-cc72-4bd4-82b6-66f379f1f00e.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDQ3Mjc4MzgsIm5iZiI6MTcwNDcyNzUzOCwicGF0aCI6Ii83NzIyNDYwNC8yOTQ5NDk0ODUtY2M4ZjgzYjMtY2M3Mi00YmQ0LTgyYjYtNjZmMzc5ZjFmMDBlLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAxMDglMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMTA4VDE1MjUzOFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTBmZmYwMDY1ZDg4MTljYTA0M2QxMTBkMGE4YWQwYjg2NjJiNjhhMTE0MWU2YTljY2ZhMmZlOWQ4N2U3YzBiY2ImWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.bRwP1RjeWQx_7ml_GOZlGFklOaW7F7pcOG2wFMU3SYw",
+    description: "BeepKart: An E-commerce website made with ReactJS, NodeJs, MongoDB and Express",
+    link: "https://flipkartclone-mern-frontend.onrender.com/",
+},
+{
+    img: "https://private-user-images.githubusercontent.com/77224604/294954058-197892d6-164b-4ece-a97b-913ec2f2d68f.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDQ3Mjg3MzgsIm5iZiI6MTcwNDcyODQzOCwicGF0aCI6Ii83NzIyNDYwNC8yOTQ5NTQwNTgtMTk3ODkyZDYtMTY0Yi00ZWNlLWE5N2ItOTEzZWMyZjJkNjhmLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAxMDglMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMTA4VDE1NDAzOFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTU1NDhjNTZkZGQ5ZjQ3ZjkxMGVlYmM3ZDlhZmIwMmQ2MjAzZTExMDM4ODU1ZjA1OWMxMzVmZDE5M2U5Yjg3M2EmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.YBv3raa0KT-IqBlxmVbwxMIZHDUlrQURt_Tlq2S8mkU",
+    description: "tindog: A tinder for dogs made with html,css and bootstrap",
+    link: "https://deaddemon.github.io/tindog/",
+},
+ 
+{
+    img: "https://private-user-images.githubusercontent.com/77224604/294952840-7b14a32d-06fc-4826-87dc-44891e6e9b6e.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDQ3Mjg0NTMsIm5iZiI6MTcwNDcyODE1MywicGF0aCI6Ii83NzIyNDYwNC8yOTQ5NTI4NDAtN2IxNGEzMmQtMDZmYy00ODI2LTg3ZGMtNDQ4OTFlNmU5YjZlLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAxMDglMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMTA4VDE1MzU1M1omWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWNhOWI1MGY2OWI4YWE1YzM2NGIxOTY0ZWIwNzJlZTY2N2MwNjhlMTE1ZTU5YWFlYjEwZjFhN2I4YTVmMWU1MmMmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.DW2k3p4az0Y3B7fMd9dBui8Vt6s6G_JRUavUoYCrx4A",
+    description: "chatbot-PWA: A progressive chat bot react App with audio recoder.",
+    link: "https://chatbot-pwa.vercel.app/",
+},
+{
+    img: "https://private-user-images.githubusercontent.com/77224604/294946848-46d5c0a6-b42d-4ace-bbb8-f123d9d5595b.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDQ3MjcyNTAsIm5iZiI6MTcwNDcyNjk1MCwicGF0aCI6Ii83NzIyNDYwNC8yOTQ5NDY4NDgtNDZkNWMwYTYtYjQyZC00YWNlLWJiYjgtZjEyM2Q5ZDU1OTViLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAxMDglMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMTA4VDE1MTU1MFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTIxNDViOGU2ZjBlMjgwNjZlNzgyZGRjZWE2OWVjMGZlZmNmMTg1ODQ4Y2ViOGI5MGZkMzNiY2ZkZTNkYjkxNzgmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.d9rzAds89G-jRyDoBLNkW-tbNNupoNgLzE8PcLF_GVc",
+    description: "theClock: An analogue clock made with html,css and javascript",
+    link: "https://deaddemon.github.io/theClock/",
+},
+ 
+{
+    img: "https://private-user-images.githubusercontent.com/77224604/294944284-6ad7ad40-e28e-47fb-9ed4-08c0a9b897c3.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDQ3MjY2NTIsIm5iZiI6MTcwNDcyNjM1MiwicGF0aCI6Ii83NzIyNDYwNC8yOTQ5NDQyODQtNmFkN2FkNDAtZTI4ZS00N2ZiLTllZDQtMDhjMGE5Yjg5N2MzLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAxMDglMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMTA4VDE1MDU1MlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWIyMDA3YTI2ZDVkNmE2NDNhZjEyZGVkZjdlMjJlYmQ3OTg4NDhhNDQwNGY2MzRkOGRjYjZiOTRlMDYxOTI1MTgmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.NXV0THlcY3hdw4bIT4Sb-wkESI1nrtqm77BQ__GybdQ",
+    description: "Songs : A music player with html,css and javascript",
+    link: "https://deaddemon.github.io/songs/",
+},
+{
+    img: "https://private-user-images.githubusercontent.com/77224604/294946108-6516d76f-6ff8-4445-9bb8-cb25a6c1551b.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDQ3MjcwNjQsIm5iZiI6MTcwNDcyNjc2NCwicGF0aCI6Ii83NzIyNDYwNC8yOTQ5NDYxMDgtNjUxNmQ3NmYtNmZmOC00NDQ1LTliYjgtY2IyNWE2YzE1NTFiLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAxMDglMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMTA4VDE1MTI0NFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTA2OGYyODI0YzdmMmI3ZDQ5NzQ4ZTA4YzNlYjQ2MmFmZTZkM2I1MDZjNmY4YmVkMjAxYjhiMzEyNWRjMmZkMjAmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.KqR9xkBfCULLdox0HKQYhEqVVKEg3pw0m7hSaIzo8cs",
+    description: "ghostyGame: An animated number game made with html,css and javascript",
+    link: "https://deaddemon.github.io/ghostyGame/",
+},
+ 
+{
+    img: "https://private-user-images.githubusercontent.com/77224604/294945618-b13aef8e-5cd5-43fa-901d-95537eef8207.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDQ3MjY5NjcsIm5iZiI6MTcwNDcyNjY2NywicGF0aCI6Ii83NzIyNDYwNC8yOTQ5NDU2MTgtYjEzYWVmOGUtNWNkNS00M2ZhLTkwMWQtOTU1MzdlZWY4MjA3LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAxMDglMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMTA4VDE1MTEwN1omWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTE3ZGNiNjMxNmU4NjVkZmQ5MjBmOWExZjM5MjgzZGIxODY3MjhhYmI2NGM1Y2VkMmQwOGE4OTgzOTQ1MTBkMmImWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.2dUdu8U9ykqPvOv_U95OStVrhE43_pYp3dky-jiL5es",
+    description: "calculator: A stylish calculator made with html,css and javascript",
+    link: "https://deaddemon.github.io/calculator/",
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "vinitapoonia333@gmail.com",
+    YOUR_FONE: "+91 7056854463",
+    description: "Happy to connect! Feel free to reach out to me for any queries. I'll try my best to get back to you! ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_h4a2rb8",
+    YOUR_TEMPLATE_ID: "template_ajwpl9m",
+    YOUR_USER_ID: "O6_yh3wEVs4uOBJDK",
+};
+
+const socialprofils = {
+    linkedin: "https://www.linkedin.com/in/vinita-6172191bb/",
+    github: "https://github.com/Deaddemon",
+    instagram: "https://instagram.com/__fl0k1__/",
+    medium: "https://medium.com/@deaddemon",
+    leetcode: "https://leetcode.com/coderdead/",
+    hackerrank: "https://www.hackerrank.com/profile/ryuk333",
+    codeforces: "https://codeforces.com/profile/0x4F5F6F",
+    codechef: "https://www.codechef.com/users/deaddemon",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
